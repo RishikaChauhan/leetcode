5d20d74fe12a0bdcbc9da7f32991f1161028cb54
RishikaChauhan <60258506+RishikaChauhan@users.noreply.github.com>
2024-04-28 08:57:52 +0530

    Time: 85 ms (78.05%) | Memory: 21.1 MB (73.58%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,32 +1,37 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # res = [[]for i in range(len(nums)+1)]
-        # counter = {}        
+        counter = {}        
+        ans = []
         
-        # for char in nums:
-        #     counter[char] = counter.get(char, 0)+1
+        for char in nums:
+            counter[char] = counter.get(char, 0)+1
 
-        # for k,v in counter.items():
-        #     res[v].append(k)
+        # for item in counter.items():
+        slis = sorted(counter.items(), key = lambda item: item[1], reverse =True)
+                # slis = sorted(lis.items(), key=lambda item: item[1], reverse=True)
+
+        for i in range(k):
+            ans.append(slis[i][0])
+        return ans
         # ret = []
         # for i in range(len(res)-1, 0, -1):
         #     for j in res[i]:
         #         ret.append(j)
         #         if len(ret)==k:
         #             return ret
-        lis = {} 
-        ans = [] 
-        for x in nums:
-            if x in lis:
-                lis[x] += 1
-            else:
-                lis[x] = 1
+        # lis = {} 
+        # ans = [] 
+        # for x in nums:
+        #     if x in lis:
+        #         lis[x] += 1
+        #     else:
+        #         lis[x] = 1
+        # print(type(lis.items()))
+        # # Step 2: Sort the dictionary items based on frequency in descending order.
+        # slis = sorted(lis.items(), key=lambda item: item[1], reverse=True)
         
-        # Step 2: Sort the dictionary items based on frequency in descending order.
-        slis = sorted(lis.items(), key=lambda item: item[1], reverse=True)
+        # # Step 3: Extract the top k frequent elements and append them to the ans list.
+        # for i in range(k):
+        #     ans.append(slis[i][0])
         
-        # Step 3: Extract the top k frequent elements and append them to the ans list.
-        for i in range(k):
-            ans.append(slis[i][0])
-        
-        return ans
+        # return ans
