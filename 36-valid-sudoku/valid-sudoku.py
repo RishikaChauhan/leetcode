20f0d33f3275a7f19164c654be4fda6b818b1bca
RishikaChauhan <60258506+RishikaChauhan@users.noreply.github.com>
2024-04-29 11:24:09 +0530

    Time: 95 ms (40.54%) | Memory: 16.6 MB (21.27%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        res=[]
+        for i in range(9):
+            for j in range(9):
+                element = board[i][j]
+                if element !='.':
+                    res +=[(i,element), (element,j),(i//3,j//3,element)]
+        return len(res)==len(set(res))
+        # print(board)
+        # for i in range(9):
+        #     if sum(list(map(int,board[i])))>45:
+        #         return False
+        
+        # for i in range(9):
+        #     sm=0
+        #     for j in range(9):
+        #         sm+=(int(board[j][i]))
+        #     if sm>45: return False
+        
