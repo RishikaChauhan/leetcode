95561050149646b9d7b555db8412cc3014c159d5
RishikaChauhan <60258506+RishikaChauhan@users.noreply.github.com>
2024-04-30 10:01:57 +0530

    Time: 505 ms (73.15%) | Memory: 18.2 MB (61.29%) - LeetSync

--- 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.py
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.py
@@ -1,9 +1,10 @@
 class Solution:
     def replaceElements(self, arr: List[int]) -> List[int]:
-        mx = arr[-1]
-        res = [0]*len(arr)
+        mx= temp = arr[-1]
+        # res = [0]*len(arr)
         for i in range(len(arr)-2, -1, -1):
-            mx = max(mx, arr[i+1])
-            res[i] = mx
-        res[-1] = -1
-        return res
+            mx = max(mx, temp)
+            temp = arr[i]
+            arr[i] = mx
+        arr[-1] = -1
+        return arr
