6a83cc472eb834b66650cf6f42fde85fe0abd9da
RishikaChauhan <60258506+RishikaChauhan@users.noreply.github.com>
2024-05-03 15:10:20 +0530

    Time: 57 ms (48.61%) | Memory: 16.6 MB (74.63%) - LeetSync

--- 965-unique-email-addresses/unique-email-addresses.py
+++ 965-unique-email-addresses/unique-email-addresses.py
@@ -2,22 +2,38 @@ class Solution:
     def numUniqueEmails(self, emails: List[str]) -> int:
         res = []
         for email in emails:
-            i = email.index('@')
-            if '+' in email:
-                new = ''
-                for char in email:
-                    if char == '+':
-                        break
-                    elif char != '.':
-                        new +=char
-                
-                res.append(new+email[i:])
-            else:
-                first = email[:i]
-                new = ''
-                for char in first:
-                    if char!='.':
-                        new+=char
-                    new_email = new+email[i:]
-                res.append(new_email)
+            local, domain = email.split("@")
+            # if '+' in local:
+            local = local.split('+')[0].replace(".","")
+            res.append(local+'@'+domain)
         return len(set(res))
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # res = []
+        # for email in emails:
+        #     i = email.index('@')
+        #     if '+' in email:
+        #         new = ''
+        #         for char in email:
+        #             if char == '+':
+        #                 break
+        #             elif char != '.':
+        #                 new +=char
+                
+        #         res.append(new+email[i:])
+        #     else:
+        #         first = email[:i]
+        #         new = ''
+        #         for char in first:
+        #             if char!='.':
+        #                 new+=char
+        #             new_email = new+email[i:]
+        #         res.append(new_email)
+        # return len(set(res))
