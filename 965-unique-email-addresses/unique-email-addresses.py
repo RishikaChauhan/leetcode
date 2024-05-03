131b01aaf22127fe77349e2f3e419aecffb97494
RishikaChauhan <60258506+RishikaChauhan@users.noreply.github.com>
2024-05-03 13:49:13 +0530

    Time: 95 ms (5.10%) | Memory: 16.9 MB (6.72%) - LeetSync

---
+++ 965-unique-email-addresses/unique-email-addresses.py
@@ -0,0 +1,24 @@
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        res = []
+        for email in emails:
+            i = email.index('@')
+            if '+' in email:
+                new = ''
+                for char in email:
+                    if char == '+':
+                        break
+                    elif char != '.':
+                        new +=char
+                
+                res.append(new+email[i:])
+            else:
+                first = email[:i]
+                new = ''
+                for char in first:
+                    if char!='.':
+                        new+=char
+                    new_email = new+email[i:]
+                res.append(new_email)
+            print(res)
+        return len(set(res))
