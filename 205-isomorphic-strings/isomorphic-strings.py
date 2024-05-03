ad0bfab840a36ed3130353a124e90493525057f5
RishikaChauhan <60258506+RishikaChauhan@users.noreply.github.com>
2024-05-03 17:00:00 +0530

    Time: 147 ms (5.33%) | Memory: 16.9 MB (25.07%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        def num_mapp(s):           
+            indx = []
+            res = ''
+            for i in s:
+                if i in indx:
+                    res+=str(indx.index(i)+1)
+                else:
+                    indx.append(i)
+                    res+=str(len(indx))
+            return res
+            print(indx,res)
+        return num_mapp(s)==num_mapp(t)
+                
