373775e3353f84a906cd0500c4c5547b84db574b
RishikaChauhan <60258506+RishikaChauhan@users.noreply.github.com>
2024-05-05 09:31:35 +0530

    Time: 161 ms (92.09%) | Memory: 17.9 MB (78.44%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,11 +1,12 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        d = {}
-        for i in nums:
-            d[i] = d.get(i,0)+1
-        mx = [0,0]
-        for k,v in d.items():
-            if v>mx[1]:
-                mx = [k,v]
-            print(mx)
-        return mx[0]
+        return collections.Counter(nums).most_common()[0][0]
+        # d = {}
+        # for i in nums:
+        #     d[i] = d.get(i,0)+1
+        # mx = [0,0]
+        # for k,v in d.items():
+        #     if v>mx[1]:
+        #         mx = [k,v]
+        #     print(mx)
+        # return mx[0]
