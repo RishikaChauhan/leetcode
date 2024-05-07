96274991a046e7abf0bc5aa1f9cdf540d342cf48
RishikaChauhan <60258506+RishikaChauhan@users.noreply.github.com>
2024-05-07 18:53:47 +0530

    Time: 41 ms (13.92%) | Memory: 16.7 MB (15.04%) - LeetSync

--- 290-word-pattern/word-pattern.py
+++ 290-word-pattern/word-pattern.py
@@ -1,21 +1,17 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
         s = s.split(" ")
-        mapChar={}
-        mapWord={}
+        d = {}
         print(set(pattern))
         if len(pattern)!= len(s): return False
         if len(set(pattern))!= len(set(s)): return False
-        for char,word in zip(pattern,s):
-            if char not in mapChar:
-                if word in mapWord:
-                    return False
-                else:
-                    mapChar[char]=word
-                    mapWord[word]=char
-            else:
-                if mapChar[char]!=word:
-                    return False
-        return True
-
+        for i in range(len(pattern)):
+            if pattern[i] not in d.keys():
+                d[pattern[i]]= [i,s[i]]
+        res1 = []
+        for i in pattern:
+            res1.append(d[i][1])
+        return res1==s
+        # print(d)
+        # return True
         
