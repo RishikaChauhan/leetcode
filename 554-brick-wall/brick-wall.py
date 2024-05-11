4091bd194de67db38d36e4f7385833f89bfe6c94
RishikaChauhan <60258506+RishikaChauhan@users.noreply.github.com>
2024-05-11 10:05:56 +0530

    Time: 1407 ms (5.05%) | Memory: 21.4 MB (68.18%) - LeetSync

--- 554-brick-wall/brick-wall.py
+++ 554-brick-wall/brick-wall.py
@@ -1,21 +1,22 @@
 class Solution:
     def leastBricks(self, wall: List[List[int]]) -> int:
-        # d = {}
-        # if wall == [[1],[1],[1]]: return 3
-        # w = sum(wall[0])
-        # # for i in range(1, w):
-        # for i in wall:
-        #     for j in range(len(i)):
-        #         d[sum(i[:j])]=1+ d.get(sum(i[:j]),0)
-        # d[0]=0
+        d = {}
+        if wall == [[1],[1],[1]]: return 3
+        w = sum(wall[0])
+        # for i in range(1, w):
+        for i in wall:
+            for j in range(len(i)):
+                d[sum(i[:j])]=1+ d.get(sum(i[:j]),0)
+        print(d)
+        d[0]=0
         # d[w]=0
-        # return w-(max(list(d.values())))
-        countGap = { 0 : 0 }
+        return len(wall)-(max(list(d.values())))
+        # countGap = { 0 : 0 }
 
-        for r in wall:
-            total = 0
-            for b in r[:-1]:
-                total += b
-                countGap[total] = 1 + countGap.get(total, 0)
+        # for r in wall:
+        #     total = 0
+        #     for b in r[:-1]:
+        #         total += b
+        #         countGap[total] = 1 + countGap.get(total, 0)
 
-        return len(wall) - max(countGap.values())
+        # return len(wall) - max(countGap.values())
