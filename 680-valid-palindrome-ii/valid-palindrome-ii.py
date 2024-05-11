fb80aba9461f4bfb1d12bc22f01ec9f97ac0dad2
RishikaChauhan <60258506+RishikaChauhan@users.noreply.github.com>
2024-05-11 20:44:12 +0530

    Time: 47 ms (99.92%) | Memory: 17.1 MB (30.20%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -13,17 +13,3 @@ class Solution:
                     or r[:i] + r[i + 1:] == s[:l - i - 1] + s[l - i:]):
                 return True
             return False
-
-        l = len(s)
-        r = s[::-1]
-        if s == r:
-            return True
-
-        for i in range(l):
-            if s[i] == r[i]:
-                continue
-            if (s[:i] + s[i + 1:] == r[:l - i - 1] + r[l - i:] 
-                    or r[:i] + r[i + 1:] == s[:l - i - 1] + s[l - i:]):
-                return True
-            
-            return False
