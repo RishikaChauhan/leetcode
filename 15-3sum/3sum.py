cf5d700e4427824ecc3a0d9722ec0352c4a96c87
RishikaChauhan <60258506+RishikaChauhan@users.noreply.github.com>
2024-05-16 08:52:00 +0530

    Time: 563 ms (87.13%) | Memory: 20.8 MB (27.29%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,28 +1,28 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
-        answer = []
-        for i in range(len(nums) - 2):
-            if nums[i] > 0:
-                break
-            if i > 0 and nums[i] == nums[i-1]:
-                continue
-            l = i + 1
-            r = len(nums) - 1
-            while l < r:
-                total = nums[i] + nums[l] + nums[r]
-                if total < 0:
-                    l += 1
-                elif total > 0:
-                    r -= 1
-                else:
-                    triplet = [nums[i], nums[l], nums[r]]
-                    answer.append(triplet)
-                    while l < r and nums[l] == triplet[1]:
-                        l += 1
-                    while l < r and nums[r] == triplet[2]:
-                        r -= 1
-        return answer
+        # nums.sort()
+        # answer = []
+        # for i in range(len(nums) - 2):
+        #     if nums[i] > 0:
+        #         break
+        #     if i > 0 and nums[i] == nums[i-1]:
+        #         continue
+        #     l = i + 1
+        #     r = len(nums) - 1
+        #     while l < r:
+        #         total = nums[i] + nums[l] + nums[r]
+        #         if total < 0:
+        #             l += 1
+        #         elif total > 0:
+        #             r -= 1
+        #         else:
+        #             triplet = [nums[i], nums[l], nums[r]]
+        #             answer.append(triplet)
+        #             while l < r and nums[l] == triplet[1]:
+        #                 l += 1
+        #             while l < r and nums[r] == triplet[2]:
+        #                 r -= 1
+        # return answer
         res = []
         nums.sort()
         target=0
@@ -30,27 +30,32 @@ class Solution:
         for i in range(len(nums)-2):
             if nums[i] > 0:
                 break
-            if nums[i] ==nums[i-1] and i!=0:
+            if nums[i] ==nums[i-1] and i>0:
                 continue
-            else:
+            # else:
                 # ilist.append(nums[i])
-                left=i+1
-                right = len(nums)-1
-                while left<right:
-                    if nums[left]==nums[left-1] and left==i+2:
-                        left=left+1
-                        continue
-                    elif right ==len(nums)-2 and nums[right]==nums[right+1]:
-                        right=right-1
-                        continue
-                    else:
-                        total = nums[left]+nums[right]+nums[i]
-                        if total== target and [nums[i],nums[left],nums[right]] not in res: 
-                            res.append([nums[i],nums[left],nums[right]])
-                            left+=1
-                            # break
-                        elif total>target:
-                            right= right-1
-                        else: left = left+1
-                    
+            left=i+1
+            right = len(nums)-1
+            while left<right:
+                # if nums[left]==nums[left-1] and left==i+2:
+                #     left=left+1
+                #     continue
+                # elif right ==len(nums)-2 and nums[right]==nums[right+1]:
+                #     right=right-1
+                #     continue
+                # else:
+                total = nums[left]+nums[right]+nums[i]
+                
+                if total== target: 
+                    t = [nums[i],nums[left],nums[right]]
+                    res.append(t)
+                    while left < right and nums[left] == t[1]:
+                        left += 1
+                    while left < right and nums[right] == t[2]:
+                        right -= 1
+                    # break
+                elif total>target:
+                    right= right-1
+                else: left = left+1
+            
         return res
