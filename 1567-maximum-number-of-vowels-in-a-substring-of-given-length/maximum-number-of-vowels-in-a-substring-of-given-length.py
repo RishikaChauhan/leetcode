a37643aa8121ca93be18b55b959a6f9e2b045957
RishikaChauhan <60258506+RishikaChauhan@users.noreply.github.com>
2024-05-26 19:52:16 +0530

    Time: 223 ms (7.54%) | Memory: 17.3 MB (61.81%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -3,12 +3,21 @@ class Solution:
         l,r = 0,k-1
         res, total = 0,0
         window = s[l:k]
-        # print(window)
+        print(window)
         count = window.count('a')+window.count('e')+window.count('i')+window.count('o')+window.count('u')
+        # print(count)
+        res = count
         for r in range(k, len(s)):
+            nc = count
+            if s[r] in ['a', 'e','i', 'o', 'u']:
+                nc+=1
             window += s[r]
+            if window[0] in ['a', 'e','i', 'o', 'u']:
+                nc-=1
             window = window[1:]
-            # print(window)
-            if count ==k: return k
-            count = max(count, window.count('a')+window.count('e')+window.count('i')+window.count('o')+window.count('u'))
-        return count
+            # print(nc, window)
+            if nc ==k: return k
+            res = max(res,nc)
+            # print(res)
+            count = nc
+        return res
