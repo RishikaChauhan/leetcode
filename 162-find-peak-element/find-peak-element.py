669a451ce0e89eaa750cacf800a036b9623e7c42
RishikaChauhan <60258506+RishikaChauhan@users.noreply.github.com>
2024-06-01 17:08:51 +0530

    Time: 44 ms (74.43%) | Memory: 16.7 MB (82.69%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -4,24 +4,24 @@ class Solution:
         if len(nums)==2: return nums.index(max(nums[0], nums[1]))
         if nums[0]>nums[1]: return 0
         if nums[-1]>nums[-2]: return len(nums)-1
-        # l = 0
-        # r = len(nums)-1
-        # while l<=r:
-        #     m = (l+r)//2
-        #     if nums[m-1] <nums[m] and nums[m]>nums[m+1]:
-        #         return m
-        #     if nums[m-1] <nums[m]: 
-        #         l = m+1
-        #     else:
-        #         r = m-1
-        # return -1
-        l, r = 0, len(nums) - 1
-        while l <= r:
-            mid = (r + l) // 2
-            if mid < len(nums) - 1 and nums[mid] < nums[mid+1]:
-                l = mid + 1
-            elif mid > 0 and nums[mid] < nums[mid-1]:
-                r = mid - 1
+        l = 0
+        r = len(nums)-1
+        while l<=r:
+            m = (l+r)//2
+            if m < len(nums)-1 and nums[m]<nums[m+1]:
+                l = m+1
+            elif m > 0 and nums[m-1] >nums[m]: 
+                r = m-1
             else:
-                return mid
+                return m
+        # return -1
+        # l, r = 0, len(nums) - 1
+        # while l <= r:
+        #     mid = (r + l) // 2
+        #     if mid < len(nums) - 1 and nums[mid] < nums[mid+1]:
+        #         l = mid + 1
+        #     elif mid > 0 and nums[mid] < nums[mid-1]:
+        #         r = mid - 1
+        #     else:
+        #         return mid
         # return mid
