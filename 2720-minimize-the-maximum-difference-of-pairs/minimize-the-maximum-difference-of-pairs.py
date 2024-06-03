372951e3404c41db1fd6e3a800c630d42cbd33cc
RishikaChauhan <60258506+RishikaChauhan@users.noreply.github.com>
2024-06-03 19:21:44 +0530

    Time: 649 ms (97.42%) | Memory: 31 MB (48.14%) - LeetSync

--- 2720-minimize-the-maximum-difference-of-pairs/minimize-the-maximum-difference-of-pairs.py
+++ 2720-minimize-the-maximum-difference-of-pairs/minimize-the-maximum-difference-of-pairs.py
@@ -2,23 +2,23 @@ class Solution:
     def minimizeMax(self, nums: List[int], p: int) -> int:
         nums.sort()
         n = len(nums)
-        def search(target):
-            total, i = 0,1
+        def search(m):
+            to, i = 0, 1
             while i<n:
-                if nums[i]-nums[i-1]<=target:
-                    total+=1
+                if nums[i]-nums[i-1]<=m:
+                    to+=1
                     i+=2
                 else:
                     i+=1
-            return total
-
+            return to
         l = 0
         r = nums[-1]-nums[0]
         while l<r:
             m = (l+r)//2
-            total = search(m)
-            if total<p:
+            t= search(m)
+            if t<p:
                 l= m+1
             else:
                 r = m
+
         return l
