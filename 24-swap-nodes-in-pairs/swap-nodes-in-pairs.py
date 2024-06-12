bee3547bb058dad553903538c665b4078d7c03a7
RishikaChauhan <60258506+RishikaChauhan@users.noreply.github.com>
2024-06-12 19:53:58 +0530

    Time: 37 ms (44.81%) | Memory: 16.5 MB (79.20%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.py
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.py
@@ -5,18 +5,21 @@
 #         self.next = next
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        lst = []
-        while head:
-            lst.append(head.val)
-            head = head.next
-        ans = []
-        for i in range(0, len(lst),2):
-            val = lst[i:i+2]
-            ans+=val[::-1]
-        final =ListNode(0)
-        temp = final
-        for i in ans:
-            temp.next = ListNode(i)
-            temp = temp.next
-        return final.next
+        dummy = ListNode(0, head)
+        prev, curr = dummy, head
 
+        while curr and curr.next:
+            # save ptrs
+            nxtPair = curr.next.next
+            second = curr.next
+
+            # reverse this pair
+            second.next = curr
+            curr.next = nxtPair
+            prev.next = second
+
+            # update ptrs
+            prev = curr
+            curr = nxtPair
+
+        return dummy.next
