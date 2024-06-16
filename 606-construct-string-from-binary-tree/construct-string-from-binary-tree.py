d33b020bec80fa2997358e4064a34e7b78570ee2
RishikaChauhan <60258506+RishikaChauhan@users.noreply.github.com>
2024-06-16 13:40:12 +0530

    Time: 46 ms (50.44%) | Memory: 17.8 MB (37.69%) - LeetSync

---
+++ 606-construct-string-from-binary-tree/construct-string-from-binary-tree.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def tree2str(self, root: Optional[TreeNode]) -> str:
+        res = []
+        self.dfs(root, res)
+        return "".join(res)
+    
+    def dfs(self, t: TreeNode, res: list):
+        # If the current node is None, do nothing and return
+        if t is None:
+            return
+        res.append(str(t.val))
+
+        # If both left and right children are None, return as there are no more branches to explore
+        if t.left is None and t.right is None:
+            return
+        res.append('(')
+
+        # Recursively call the DFS function for the left child
+        self.dfs(t.left, res)
+        res.append(')')
+        if t.right is not None:
+            res.append('(')
+
+            # Recursively call the DFS function for the right child
+            self.dfs(t.right, res)
+            res.append(')')
+            
+
