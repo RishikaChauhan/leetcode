5c4390846c9a740a1c39bc5a0d7aff0aeb706174
RishikaChauhan <60258506+RishikaChauhan@users.noreply.github.com>
2024-06-22 16:12:44 +0530

    Time: 31 ms (78.92%) | Memory: 16.5 MB (20.69%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.py
+++ 96-unique-binary-search-trees/unique-binary-search-trees.py
@@ -1,5 +1,23 @@
 class Solution:
     def numTrees(self, n: int) -> int:
+        # def solve(n):
+        #     if n<=1:
+        #         return 1
+        #     if n in memo:
+        #         return memo[n]
+        #     ans = 0
+        #     for i in range(1,n+1):
+        #         left= solve(i-1)
+        #         right = solve(n-i)
+        #         ans+=left*right
+        #     memo[n] = ans
+        #     return memo[n]
+        # memo = {}
+        # return solve(n)
+        
+        
+        
+        
         def solve(n):
             if n <= 1:
                 return 1
