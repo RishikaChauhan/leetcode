c07252ed629837856a99d0ad1110d64843759b6a
RishikaChauhan <60258506+RishikaChauhan@users.noreply.github.com>
2024-08-01 09:11:14 +0530

    Time: 54 ms (76.29%) | Memory: 16.6 MB (25.68%) - LeetSync

--- 1547-destination-city/destination-city.py
+++ 1547-destination-city/destination-city.py
@@ -1,6 +1,16 @@
 class Solution:
     def destCity(self, paths: List[List[str]]) -> str:
-        sec = set(path[1] for path in paths)
-        fir = set(path[0] for path in paths)
-        ans = sec-fir
-        return ans.pop()
+        # sec = set(path[1] for path in paths)
+        # fir = set(path[0] for path in paths)
+        # ans = sec-fir
+        # # return ans.pop()
+
+        dest = {}
+        source = {}
+        for i in paths:
+            dest[i[1]] = 1
+            source[i[0]] = 1
+        print(dest, source)
+        for i in dest:
+            if i not in source:
+                return i
