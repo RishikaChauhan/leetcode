3734069305be9dd462a198ce3dcddf4560c2801c
RishikaChauhan <60258506+RishikaChauhan@users.noreply.github.com>
2024-09-05 16:19:06 +0530

    Time: 57 ms (55.93%) | Memory: 16.5 MB (93.20%) - LeetSync

--- 1547-destination-city/destination-city.py
+++ 1547-destination-city/destination-city.py
@@ -4,13 +4,31 @@ class Solution:
         # fir = set(path[0] for path in paths)
         # ans = sec-fir
         # # return ans.pop()
+        s,d = [],[]
 
-        dest = {}
-        source = {}
         for i in paths:
-            dest[i[1]] = 1
-            source[i[0]] = 1
-        print(dest, source)
-        for i in dest:
-            if i not in source:
-                return i
+            s.append(i[0])
+            # d.append(i[1])
+        # print(s, d)
+        for i in paths:
+            if i[1] not in s:
+                return i[1]
+
+
+
+
+
+
+
+
+
+
+        # dest = {}
+        # source = {}
+        # for i in paths:
+        #     dest[i[1]] = 1
+        #     source[i[0]] = 1
+        # # print(dest, source)
+        # for i in dest:
+        #     if i not in source:
+        #         return i
