4450a6b089c3edd1ae18fda8da6ac321649ebf94
RishikaChauhan <60258506+RishikaChauhan@users.noreply.github.com>
2025-01-02 18:11:47 +0530

    Time: 3 ms (91.95%) | Memory: 21 MB (42.23%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,37 +1,8 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        counter = {}        
-        ans = []
-        
-        for char in nums:
-            counter[char] = counter.get(char, 0)+1
-
-        # for item in counter.items():
-        slis = sorted(counter.items(), key = lambda item: item[1], reverse =True)
-                # slis = sorted(lis.items(), key=lambda item: item[1], reverse=True)
-
-        for i in range(k):
-            ans.append(slis[i][0])
-        return ans
-        # ret = []
-        # for i in range(len(res)-1, 0, -1):
-        #     for j in res[i]:
-        #         ret.append(j)
-        #         if len(ret)==k:
-        #             return ret
-        # lis = {} 
-        # ans = [] 
-        # for x in nums:
-        #     if x in lis:
-        #         lis[x] += 1
-        #     else:
-        #         lis[x] = 1
-        # print(type(lis.items()))
-        # # Step 2: Sort the dictionary items based on frequency in descending order.
-        # slis = sorted(lis.items(), key=lambda item: item[1], reverse=True)
-        
-        # # Step 3: Extract the top k frequent elements and append them to the ans list.
-        # for i in range(k):
-        #     ans.append(slis[i][0])
-        
-        # return ans
+        d = {}
+        for i in nums:
+	        d[i] = d.get(i, 0)+1
+        d = dict(sorted(d.items(), key=lambda item: item[1], reverse=True))
+        print(list(d.keys()))
+        return list(d.keys())[:k]
