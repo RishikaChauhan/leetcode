606beb27579e739121b75b894b09a7c0d8d2b2b3
RishikaChauhan <60258506+RishikaChauhan@users.noreply.github.com>
2025-01-03 17:58:15 +0530

    Time: 35 ms (26.80%) | Memory: 23.2 MB (53.85%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,24 +1,58 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        prod = 1
-        flag = 0
-        if nums == [0]*len(nums): return nums
-        if nums.count(0)>1: return [0]*len(nums)
-        for i in range(len(nums)):
-            if nums[i]!=0:
-                prod *=nums[i]
-            else:
-                flag = 1
-                continue
-        res = []
-        if flag == 1:
+        p=1
+        cz = 0
+        if nums.count(0)==1:
+            k =nums.index(0) 
+            cz = 1
+        elif nums.count(0)>1:
+            cz = 2
 
-            for i in range(len(nums)):
-                if nums[i]==0:
-                    res.append(prod)
-                else:
-                    res.append(0)
+        for i in range(len(nums)):
+            if nums[i]==0:
+                continue
+            else:
+                p = p*nums[i]
+        
+        if cz>1:
+            return [0]*len(nums)
+        elif cz==1:
+            l = [0]*len(nums)
+            l[k] = p
+            return l
         else:
-            for i in range(len(nums)):
-                res.append(prod//nums[i])
-        return res
+            return [(lambda x: p // x)(num) for num in nums]
+
+        
+
+# from typing import List
+
+# class Solution:
+#     def productExceptSelf(self, nums: List[int]) -> List[int]:
+#         p = 1
+#         zero_count = 0
+        
+#         # Count the number of zeros in the array
+#         for num in nums:
+#             if num == 0:
+#                 zero_count += 1
+#             else:
+#                 p *= num
+        
+#         # If more than one zero, all products will be zero
+#         if zero_count > 1:
+#             return [0] * len(nums)
+        
+#         result = [0] * len(nums)
+        
+#         # If there's exactly one zero, set product for that position only
+#         if zero_count == 1:
+#             zero_index = nums.index(0)
+#             result[zero_index] = p
+#             return result
+        
+#         # If there are no zeros, calculate the product for each non-zero position
+#         for i in range(len(nums)):
+#             result[i] = p // nums[i]
+
+#         return result
