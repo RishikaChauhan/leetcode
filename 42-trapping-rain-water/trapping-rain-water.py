cd04b95f1861b255f39ec7101630be0cbd5aef36
RishikaChauhan <60258506+RishikaChauhan@users.noreply.github.com>
2025-01-05 12:45:45 +0530

    Time: 15 ms (54.04%) | Memory: 19.4 MB (14.14%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,22 +1,25 @@
+from typing import List
+
 class Solution:
     def trap(self, height: List[int]) -> int:
+        # Edge case: if the list is empty or has less than 3 bars, no water can be trapped
+        if not height or len(height) < 3:
+            return 0
+
         count = 0
-        rheight= height[::-1]
-        # k = sorted(list(set(height)), reverse = True)
-        
-        l =0
-        r = len(height)-1
-        lh = height[l]
-        rh = height[r]
-        while l<r:
-            if lh<rh:
-                l+=1
-                lh = max(lh, height[l])
-                count +=lh-height[l]
+        left = 0
+        right = len(height) - 1
+        left_max = height[left]
+        right_max = height[right]
+
+        while left < right:
+            if height[left] < height[right]:
+                left += 1
+                left_max = max(left_max, height[left])
+                count += left_max - height[left]
             else:
-                r -= 1
-                rh = max(rh, height[r])
-                count += rh - height[r]
-            
+                right -= 1
+                right_max = max(right_max, height[right])
+                count += right_max - height[right]
+
         return count
-            
