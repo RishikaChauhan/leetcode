c24e6e04018564495577e17a98556cf36c4e3c40
RishikaChauhan <60258506+RishikaChauhan@users.noreply.github.com>
2025-02-17 23:37:56 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (53.02%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -1,13 +1,21 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        r = nums.count(0)
-        w = nums.count(1)
-        b = nums.count(2)
-        for i in range(0,r):
-            nums[i]=0
-        for i in range(r,r+w):
-            nums[i]=1
-        for i in range(r+w, len(nums)):
-            nums[i]= 2
-        return nums
-        
+        d = {}
+    
+    # Count occurrences of 0, 1, and 2
+        for i in nums:
+            d[i] = 1 + d.get(i, 0)
+
+        # Ensure missing keys default to 0
+        d[0] = d.get(0, 0)
+        d[1] = d.get(1, 0)
+        d[2] = d.get(2, 0)
+
+        # Overwrite nums in sorted order
+        for i in range(d[0]):
+            nums[i] = 0
+        for j in range(d[0], d[0] + d[1]):
+            nums[j] = 1
+        for k in range(d[0] + d[1], len(nums)):
+            nums[k] = 2
+
