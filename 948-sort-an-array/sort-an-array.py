8ad5be7c6f0fe6d2fa029aad44c7e04981267476
RishikaChauhan <60258506+RishikaChauhan@users.noreply.github.com>
2025-02-17 22:45:16 +0530

    Time: 630 ms (56.54%) | Memory: 25.5 MB (45.08%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -1,15 +1,29 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        def quicksort(nums):
-            if len(nums) <= 1: return nums
-            
-            #picking a random pivot
-            pivot = random.choice(nums)
-            less_than, equal_to, greater_than = [], [], []
-            
-            for val in nums:
-                if val < pivot: less_than.append(val)
-                elif val > pivot: greater_than.append(val)
-                else: equal_to.append(val)
-            return quicksort(less_than) + equal_to + quicksort(greater_than)
-        return quicksort(nums)
+        
+
+        def merge_sort(arr):
+            if len(arr)<=1:
+                return arr
+            mid = len(arr)//2
+            mid_left = merge_sort(arr[:mid])
+            mid_right = merge_sort(arr[mid:])
+            return merge(mid_left, mid_right)
+
+        def merge(l,r):
+            sorted_list = []
+            i = 0
+            j = 0
+            while i<len(l) and j<len(r):
+                if l[i]<r[j]:
+                    sorted_list.append(l[i])
+                    i+=1
+                else:
+                    sorted_list.append(r[j])
+                    j+=1
+            sorted_list.extend(l[i:])
+            sorted_list.extend(r[j:])
+
+            return sorted_list
+
+        return merge_sort(nums)
