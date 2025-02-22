4250a4c1212f2c952a4b34741e9b915357d81e59
RishikaChauhan <60258506+RishikaChauhan@users.noreply.github.com>
2025-02-22 18:46:54 +0530

    Time: 11 ms (4.15%) | Memory: 17.8 MB (50.96%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -3,3 +3,17 @@ class Solution:
         while ('{}' in s) or ('[]' in s) or ('()' in s):
             s = s.replace("{}", "").replace("[]","").replace("()","")
         return s==""
+
+
+        stack = []
+        for i in s:
+            p = stack[-1]
+            if i == '}' and p == '{':
+                stack.pop()
+            if i == ')' and p == '(':
+                stack.pop()
+            if i == ']' and p == '[':
+                stack.pop()
+            else:
+                stack.append(i)
+        return stack==[]
