c11848cde8a3e92ba608258fcc49b6089cc48554
RishikaChauhan <60258506+RishikaChauhan@users.noreply.github.com>
2025-02-28 15:52:29 +0530

    Time: 2 ms (73.30%) | Memory: 18.8 MB (89.26%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -10,6 +10,8 @@ class Solution:
         def height(root):
             if not root: return 0
             leftheight, rightheight = height(root.left), height(root.right)
+            # return 1+ max(leftheight, rightheight)
             if leftheight<0 or rightheight<0 or abs(leftheight-rightheight)>1: return -1
             return max(leftheight, rightheight)+1
-        return height(root)>=0
+        if not root: return True
+        return (height(root))>=0
