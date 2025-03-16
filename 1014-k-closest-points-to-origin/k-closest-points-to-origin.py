ec2e0c8900aa35012117e601425af2633a33bd9b
RishikaChauhan <60258506+RishikaChauhan@users.noreply.github.com>
2025-03-16 23:42:16 +0530

    Time: 65 ms (64.83%) | Memory: 22.9 MB (36.06%) - LeetSync

---
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -0,0 +1,20 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        h = {}  # Dictionary to store distances and points
+        
+        for i in points:
+            o = (i[0]**2 + i[1]**2)  # Fixed exponentiation issue
+            if o in h:  
+                h[o].append(i)  # Handle duplicate distances properly
+            else:
+                h[o] = [i]
+
+        l = list(h.keys())  # Get list of distances
+        heapq.heapify(l)  # Min-heap based on distances
+        res = []
+
+        while len(res) < k:
+            closest_dist = heapq.heappop(l)  # Get the smallest distance
+            res.extend(h[closest_dist])  # Add all points with this distance
+
+        return res[:k]
