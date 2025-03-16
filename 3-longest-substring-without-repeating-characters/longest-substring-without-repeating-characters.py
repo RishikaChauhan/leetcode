6d3a1987c84a75159dcc8da9c54e24fdef6a114b
RishikaChauhan <60258506+RishikaChauhan@users.noreply.github.com>
2025-03-16 16:15:29 +0530

    Time: 23 ms (32.69%) | Memory: 18 MB (12.70%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,19 +1,31 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        i = 0
-        res = ""
-        mcount = 0
-        count = 0
-        while i<len(s):
-            if s[i] not in res:
-                res+=s[i]
-                count+=1
-                i+=1
-                mcount = max(len(res), mcount)
-                
+        # i = j = 0
+        # ss = ''
+        # res = 0
+        # while j<len(s):
+        #     # ss = s[i:j]
+        #     # res = max(len(ss), res)
+        #     if s[j] in ss:
+        #         i+=1
+        #         ss = ss[i:j]
+        #     else:
+        #         ss+=s[j]
+        #         res = max(res, len(ss))
+        #         j+=1
+
+        # return res
+        i = j = 0
+        ss = ''
+        res = 0
+        
+        while j < len(s):  # Corrected loop condition
+            if s[j] in ss:  # Check if character exists in substring
+                i += 1
+                ss = s[i:j]  # Update substring by removing leftmost character
             else:
-                
-                res = res[1:]
-                
-            # mcount = max(count, mcount)
-        return mcount
+                ss += s[j]  # Add current character
+                res = max(res, len(ss))  # Update max length
+                j += 1  # Move forward
+        
+        return res
