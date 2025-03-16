768359e81b1f1efc76fbe6a4fb400afc7d7b8089
RishikaChauhan <60258506+RishikaChauhan@users.noreply.github.com>
2025-03-16 16:16:04 +0530

    Time: 20 ms (41.09%) | Memory: 17.9 MB (34.41%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,31 +1,31 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        # i = j = 0
-        # ss = ''
-        # res = 0
-        # while j<len(s):
-        #     # ss = s[i:j]
-        #     # res = max(len(ss), res)
-        #     if s[j] in ss:
-        #         i+=1
-        #         ss = ss[i:j]
-        #     else:
-        #         ss+=s[j]
-        #         res = max(res, len(ss))
-        #         j+=1
-
-        # return res
         i = j = 0
         ss = ''
         res = 0
-        
-        while j < len(s):  # Corrected loop condition
-            if s[j] in ss:  # Check if character exists in substring
-                i += 1
-                ss = s[i:j]  # Update substring by removing leftmost character
+        while j<len(s):
+            # ss = s[i:j]
+            # res = max(len(ss), res)
+            if s[j] in ss:
+                i+=1
+                ss = s[i:j]
             else:
-                ss += s[j]  # Add current character
-                res = max(res, len(ss))  # Update max length
-                j += 1  # Move forward
-        
+                ss+=s[j]
+                res = max(res, len(ss))
+                j+=1
+
         return res
+        # i = j = 0
+        # ss = ''
+        # res = 0
+        
+        # while j < len(s):  # Corrected loop condition
+        #     if s[j] in ss:  # Check if character exists in substring
+        #         i += 1
+        #         ss = s[i:j]  # Update substring by removing leftmost character
+        #     else:
+        #         ss += s[j]  # Add current character
+        #         res = max(res, len(ss))  # Update max length
+        #         j += 1  # Move forward
+        
+        # return res
