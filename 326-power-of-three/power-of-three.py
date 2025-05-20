fbf668bfed884482430e8761fd7e0cde4b52786f
RishikaChauhan <60258506+RishikaChauhan@users.noreply.github.com>
2025-05-20 13:02:11 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (72.84%) - LeetSync

--- 326-power-of-three/power-of-three.py
+++ 326-power-of-three/power-of-three.py
@@ -1,6 +1,7 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
         
+        return n>0 and 3**19 %n==0
         # def div(n):
         if n<=0: return False
         while n%3 ==0:
