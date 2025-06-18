cce745b76bed23e9d92f2b0e8bc98c16aad48136
RishikaChauhan <60258506+RishikaChauhan@users.noreply.github.com>
2025-06-18 13:28:36 +0530

    Time: 266 ms (35.02%) | Memory: 29.2 MB (5.99%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,24 +1,61 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid: return 0
-        visit=set()
         island = 0
-        def dfs(row, col):
-            if (row,col) not in visit:
-                if row==-1 or row==len(grid): return
-                if col ==-1 or col ==len(grid[0]): return
+        visit = set()
+        def dfs(m,n):
+            if (m,n) not in visit:
+                # if row==-1 or row==len(grid): return
+                # if col ==-1 or col ==len(grid[0]): return
+                if -1<m<len(grid) and -1<n<len(grid[0]):
+                    if grid[m][n]=="1":
+                        visit.add((m,n))
+                        dfs(m-1, n)
+                        dfs(m+1, n)
+                        dfs(m,n-1)
+                        dfs(m,n+1)
+                        # island+=1
+                
+            else: return
+        #     if (row,col) not in visit:
+        #         if row==-1 or row==len(grid): return
+        #         if col ==-1 or col ==len(grid[0]): return
         
-                if grid[row][col] =="1":
+        #         if grid[row][col] =="1":
                     
-                    visit.add((row, col))
-                    dfs(row+1, col)
-                    dfs(row-1, col)
-                    dfs(row, col-1)
-                    dfs(row, col+1)
-        for row in range(len(grid)):
-                    
-            for col in range(len(grid[0])):
-                if (row, col) not in visit and grid[row][col]=="1":
-                    dfs(row, col)
+        #             visit.add((row, col))
+        #             dfs(row+1, col)
+        #             dfs(row-1, col)
+        #             dfs(row, col-1)
+        #             dfs(row, col+1)
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if (i,j) not in visit and grid[i][j] == "1":
+                    dfs(i,j)
                     island+=1
+                    
         return island
+        
+        # if not grid: return 0
+        # visit=set()
+        # island = 0
+        # def dfs(row, col):
+        #     if (row,col) not in visit:
+        #         if row==-1 or row==len(grid): return
+        #         if col ==-1 or col ==len(grid[0]): return
+        
+        #         if grid[row][col] =="1":
+                    
+        #             visit.add((row, col))
+        #             dfs(row+1, col)
+        #             dfs(row-1, col)
+        #             dfs(row, col-1)
+        #             dfs(row, col+1)
+        # for row in range(len(grid)):
+                    
+        #     for col in range(len(grid[0])):
+        #         if (row, col) not in visit and grid[row][col]=="1":
+        #             dfs(row, col)
+        #             island+=1
+        # return island
