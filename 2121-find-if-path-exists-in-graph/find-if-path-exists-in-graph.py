739d5a3285e26b65b54ac690736ca1dfd912fd01
RishikaChauhan <60258506+RishikaChauhan@users.noreply.github.com>
2025-06-22 17:45:19 +0530

    Time: 610 ms (39.70%) | Memory: 139.9 MB (20.74%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
@@ -1,39 +1,18 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        graph  = {}
-        def build_graph(edg):
-            for edge in edg:
-                a, b = edge
-                if a not in graph:
-                    graph[a] = []
-                if b not in graph:
-                    graph[b] = []
-                graph[a].append(b)
-                graph[b].append(a)
-            return graph       
-        
-        
-        visit=set()
-        # def dfs(node):
-        #     if not node: return False
-        #     if node in visit: return False
-        #     if destination in edges[node]:
-        #         return True
-        #     for i in edges[node]:
-        #         dfs(i)
-        #         visit.add(i)
-        #     return False
-
-
-        g = build_graph(edges)
-        def dfs(grap, src, dest, visit):
-            if src == dest: return True
-            if src in visit:
-                return False
-            visit.add(src)
-            for nei in graph[src]:
-                if dfs(grap, nei, dest, visit):
+        neigh = defaultdict(list)
+        for n1, n2 in edges:
+            neigh[n1].append(n2)
+            neigh[n2].append(n1)
+            
+        def dfs(node):
+            if node ==destination: return True
+            if node in seen: return False
+            seen.add(node)
+            for n in neigh[node]:
+                if dfs(n):
                     return True
             return False
-        return dfs(g, source, destination, visit)
-                
+
+        seen = set()
+        return dfs(source)
