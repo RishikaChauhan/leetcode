dc2eee594b630ee426beee580ecbfd8d71334300
RishikaChauhan <60258506+RishikaChauhan@users.noreply.github.com>
2025-07-04 16:29:11 +0530

    Time: 3 ms (89.12%) | Memory: 19.7 MB (55.33%) - LeetSync

--- 337-house-robber-iii/house-robber-iii.py
+++ 337-house-robber-iii/house-robber-iii.py
@@ -5,22 +5,11 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    # def rob(self, root: Optional[TreeNode]) -> int:
-        # def dfs(root):
-        #     if not root: return (0,0)
-        # left = dfs(root.left)
-        # right = dfs(root.right)
-        # return (root.val+left[1]+right[1], max(left[0], left[1])+max(right[0], right[1]))
-
-        
-        
-        # return max(dfs(root))
-    def rob(self, root: TreeNode) -> int:
-        return max(self.dfs(root))
-    
-    def dfs(self, root: TreeNode):
-        if not root:
-            return (0, 0)
-        left = self.dfs(root.left)
-        right = self.dfs(root.right)
-        return (root.val + left[1] + right[1], max(left[0], left[1]) + max(right[0], right[1]))
+    def rob(self, root: Optional[TreeNode]) -> int:
+        def dfs(root):
+            if not root:
+                return (0,0)
+            left = dfs(root.left)
+            right = dfs(root.right)
+            return (root.val + left[1] + right[1], max(left[0], left[1]) + max(right[0], right[1]))
+        return max(dfs(root))
